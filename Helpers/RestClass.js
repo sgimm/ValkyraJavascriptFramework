18efaf28a7872e045e2fd30d0409a189eb3d3124
sgimm <gimm@valkyra.de>
2018-06-29 21:42:55 +0200

    Class sep2

---
+++ Helpers/RestClass.js
@@ -0,0 +1,26 @@
+﻿class RestClass {
+    constructor(url, Method) {
+        this.ClassName = "RestClass";
+        this._http = new XMLHttpRequest();
+        this._url = url;
+        this._method = Method;
+    }
+    Request(data, callback) {
+        this._http.open(this._method, this._url, true);
+        this._http.responseType = "json";
+        this._http.onreadystatechange = function () {
+            if (this._http.readyState == 4 && this._http.status == 200)
+                callback(this._http.response);
+        }
+        this._http.send(data);
+    };
+    RequestFile(data, callback) {
+        this._http = new XMLHttpRequest();
+        this._http.open(this._method, this._url, true);
+        this._http.onreadystatechange = x => {
+            if (this._http.readyState == 4 && this._http.status == 200)
+                callback(this._http.response);
+        }
+        this._http.send(data);
+    }
+}
